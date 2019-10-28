090f276a8230e90124c237ff2f6aa5dc6bb2e95d
hswaminathan <hswaminathan@aep.com>
2019-10-28 17:26:22 -0400

    new method added

--- ConsoleApp1/ConsoleApp1/Class1.cs
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -9,6 +9,15 @@ namespace ConsoleApp1
     class Class1
     {
         string x = "hari added";
-       string yy = "Editing in Github";
+        string yy = "Editing in Github";
+        public int Add()
+        {
+            Int32 x1 = 1;
+            Int32 x2 = 2;
+            int sum = x1 + x2;
+            return sum;
+        }
+
     }
+
 }
