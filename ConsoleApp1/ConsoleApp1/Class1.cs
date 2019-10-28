fa7c123bab332111b2263f42754a3eab6d8dc062
hswaminathan <hswaminathan@aep.com>
2019-10-28 17:34:47 -0400

    c4

--- ConsoleApp1/ConsoleApp1/Class1.cs
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         string x = "hari added";
         string yy = "Editing in Github";
-        public int Add()
+        public int Add(int x1,int x2)
         {
             return   x1 + x2;            
         }
